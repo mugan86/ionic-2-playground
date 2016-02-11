049f1b4ecf71507d1966fd0dbb55c081b272eea1
Anartz Mugika <mugan86@gmail.com>
2016-02-11 13:34:31 +0100

    Update components-ionic-2

---
+++ app/pages/components-ionic-2/components-ionic-2.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic/ionic';
+
+
+@Page({
+  templateUrl: 'build/pages/components-ionic-2/components-ionic-2.html'
+})
+export class ComponentsIonicTwoPage {
+  constructor() {
+    console.log("Ionic 2 Framework 2 Components!!!");
+  }
+}
