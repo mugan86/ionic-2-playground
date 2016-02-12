fdbce17824856508151b5f2ce36390619450c792
Anartz Mugika <mugan86@gmail.com>
2016-02-12 14:13:05 +0100

    Get list with random values

--- app/pages/race-list/race-list.js
+++ app/pages/race-list/race-list.js
@@ -13,16 +13,33 @@ export class RaceListPage {
 
     //Call API to show race list in current year
     this.loadRaceList();
+    this.randomValues();
+
+    this.images = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
   }
 
 
   loadRaceList()
   {
+    //Get F1 current year races
     this.raceService.searchRacesCurrentYear().subscribe(
       data => {this.races = data.MRData.RaceTable.Races; console.log(this.races[0].season); this.current = this.races[0].season},
       err => this.logError(err),
-      () => console.log('Loading finish')
+      () => console.log('Loading finish');
     );
+  }
+
+  randomValues ()
+  {
+    var arr = []
+
+    while(arr.length < 20){
+      var randomnumber=Math.ceil(Math.random()*20);
+      arr.push(randomnumber);
+    }
+    console.log(arr);
+    return arr;
   }
 
 
