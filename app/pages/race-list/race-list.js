1e932b6913f840d1033a859b8f4da687af9e0444
Anartz Mugika <mugan86@gmail.com>
2016-02-15 13:28:26 +0100

    Add select item action in layout and redirect to select race page

--- app/pages/race-list/race-list.js
+++ app/pages/race-list/race-list.js
@@ -1,5 +1,6 @@
 import {Page, NavController} from 'ionic/ionic';
 import {RaceService} from '../services/RaceService';
+import {SelectRaceDetailsPage} from '../race-details/race-details';
 
 @Page({
   templateUrl: 'build/pages/race-list/race-list.html',
@@ -7,53 +8,56 @@ import {RaceService} from '../services/RaceService';
 })
 
 export class RaceListPage {
-  constructor(nav: NavController, raceService:RaceService) {
-    this.nav = nav;
-    this.raceService = raceService;
+    constructor(nav: NavController, raceService:RaceService) {
+        this.nav = nav;
+        this.raceService = raceService;
 
-    this.images = [
-                    'advance-card-bttf',
-                    'advance-card-map-madison',
-                    'advance-card-map-mario',
-                    'avatar-amber',
-                    'avatar-ben',
-                    'avatar-cher',
-                    'avatar-dionne',
-                    'avatar-han',
-                    'marty-avatar',
-                    'avatar-travis',
-                    'avatar-tai'
-                    ];
+        this.images = [
+                        'advance-card-bttf',
+                        'advance-card-map-madison',
+                        'advance-card-map-mario',
+                        'avatar-amber',
+                        'avatar-ben',
+                        'avatar-cher',
+                        'avatar-dionne',
+                        'avatar-han',
+                        'marty-avatar',
+                        'avatar-travis',
+                        'avatar-tai'
+                        ];
 
-    //Call API to show race list in current year
-    this.loadRaceList();
-    this.avatars = this.randomValues();
-
-
-  }
-
-
-  loadRaceList()
-  {
-    //Get F1 current year races
-    this.raceService.searchRacesCurrentYear().subscribe(
-      data => {this.races = data.MRData.RaceTable.Races; console.log(this.races[0].season); this.current = this.races[0].season},
-      err => this.logError(err),
-      () => console.log('Loading finish');
-    );
-  }
-
-  randomValues ()
-  {
-    arr = []
-
-    while(arr.length < 20){
-      var randomnumber=Math.ceil(Math.random()*(this.images.length - 1));
-      arr.push(randomnumber);
+        //Call API to show race list in current year
+        this.loadRaceList();
+        this.avatars = this.randomValues();
     }
-    console.log(arr);
-    return arr;
+
+
+    loadRaceList()
+    {
+        //Get F1 current year races
+        this.raceService.searchRacesCurrentYear().subscribe(
+          data => {this.races = data.MRData.RaceTable.Races; console.log(this.races[0]); this.current = this.races[0].season},
+          err => this.logError(err),
+          () => console.log('Loading finish');
+        );
+    }
+
+    randomValues ()
+    {
+        arr = []
+
+        while(arr.length < 20){
+          var randomnumber=Math.ceil(Math.random()*(this.images.length - 1));
+          arr.push(randomnumber);
+        }
+        console.log(arr);
+        return arr;
+    }
+
+    itemTapped(event, item) {
+        console.log("Click");
+     this.nav.push(SelectRaceDetailsPage, {
+       item: item
+     });
   }
-
-
 }
