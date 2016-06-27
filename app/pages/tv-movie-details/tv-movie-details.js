89a8d22a8cb7e702b824b181ab649d2847e67c76
Anartz Mugika <mugan86@gmail.com>
2016-06-27 11:36:55 +0200

    Add details page to show and manage select tv show details and show

---
+++ app/pages/tv-movie-details/tv-movie-details.js
@@ -0,0 +1,30 @@
+import {Page, NavController, NavParams} from 'ionic/ionic';
+import {TheMovieDBAPIService} from '../services/TheMovieDBAPIService';
+
+
+@Page({
+  templateUrl: 'build/pages/tv-movie-details/tv-movie-details.html',
+  providers: [TheMovieDBAPIService]
+})
+export class SelectTVMovieDetailsPage {
+  constructor(nav: NavController, navParams: NavParams, movieService:TheMovieDBAPIService) {
+
+  	this.movieService = movieService;
+    this.nav = nav;
+    this.details = [];
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+
+    console.log("choose: " + this.selectedItem.id);
+
+    this.movieService.getMovieMoreInfo(this.selectedItem.id).subscribe(
+          data => {this.details = data; console.log(this.details.number_of_episodes);},
+          err => this.logError(err),
+          () => console.log('Loading 2010 finish');
+        );
+  }
+
+  goToHomepage(url) {
+     console.log(url);
+  }
+}
